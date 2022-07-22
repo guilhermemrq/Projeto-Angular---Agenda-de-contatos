454a305460cbcf9c12427ba06cd3e7a171767e98
guilhermem <redes.guilherme@gmail.com>
2022-07-22 08:23:59 -0300

    primeiro commit

---
+++ src/app/contato/contato.component.ts
@@ -0,0 +1,94 @@
+import { Component, OnInit } from '@angular/core';
+import { ContatoService } from '../contato.service';
+import { Contato } from './contato';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { MatDialog } from '@angular/material/dialog'
+
+import { ContatoDetalheComponent } from '../contato-detalhe/contato-detalhe.component'
+import { PageEvent } from '@angular/material/paginator';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Component({
+  selector: 'app-contato',
+  templateUrl: './contato.component.html',
+  styleUrls: ['./contato.component.css']
+})
+export class ContatoComponent implements OnInit {
+
+  formulario: FormGroup;
+  contatos: Contato[] = [];
+  colunas = ['foto','id','nome','email','favorito'];
+  totalElementos = 0;
+  pagina = 0;
+  tamanho = 10;
+  pageSizeOptions: number[] = [10]
+
+  constructor(private service: ContatoService, private fb: FormBuilder, private dialog: MatDialog, private snackBar: MatSnackBar) { }
+
+  ngOnInit(): void {
+    this.montarFormulario();
+    this.listarContatos(this.pagina, this.tamanho);
+  }
+
+  montarFormulario(){
+    this.formulario = this.fb.group({
+      nome: ['', Validators.required],
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  listarContatos(pagina = 0, tamanho = 10){
+    this.service.list(pagina, tamanho)
+        .subscribe(response =>{
+          this.contatos = response.content;
+          this.totalElementos = response.totalElements;
+          this.pagina = response.number;
+        })
+  }
+
+  paginar(event: PageEvent){  
+    this.pagina = event.pageIndex;
+    this.listarContatos(this.pagina, this.tamanho);
+  }
+
+  favoritar(contato: Contato){
+    this.service.favourite(contato)
+        .subscribe(response =>{
+          contato.favorito = !contato.favorito;
+        })
+  }
+
+  uploadFoto(event: any, contato: Contato){
+    const files = event.target.files;
+    if(files){
+      const foto = files[0];
+      const formData: FormData = new FormData();
+      formData.append("foto", foto);
+      this.service.upload(contato, formData)
+          .subscribe(response =>{
+            this.listarContatos(this.pagina, this.tamanho);
+          })
+    }
+  }
+
+  visualizarContato(contato: Contato){
+    this.dialog.open(ContatoDetalheComponent, {
+      width: '400px',
+      height: '450px',
+      data: contato
+    })
+  }
+
+  submit(){
+    const formValues = this.formulario.value;
+    const contato: Contato = new Contato(formValues.nome, formValues.email);
+    this.service.save(contato)
+        .subscribe(response =>{
+          this.listarContatos(0, 10);
+          this.snackBar.open('O Contato foi adcionado!','Sucesso!',{
+            duration: 2000
+          });
+          this.formulario.reset();
+        })
+  }
+}
